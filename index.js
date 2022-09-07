5e6f06adf792d1c2ea1041dee8c3a67e143b8ce2
Ambar <Ambarpg80@gmail.com>
2022-09-07 18:11:22 -0400

    commit tree f6ba90e26e1ee12863ecba5c02a6899734262e61

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase();
+}
+//upperCaseCustomerName();
+
+ 
+function setBestCustomer(){
+     
+   bestCustomer = "not bob"
+}
+//setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob';
+}
+//overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'Wes';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Robin';
+}
+    
